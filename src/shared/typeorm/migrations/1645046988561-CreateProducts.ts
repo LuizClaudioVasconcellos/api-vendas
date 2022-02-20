5d912c838a202ff57aace939663bccf8828eae9f
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2022-02-20 17:07:37 -0300

    Create products entitie

--- src/shared/typeorm/migrations/1645046988561-CreateProducts.ts
+++ src/shared/typeorm/migrations/1645046988561-CreateProducts.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateProducts1645046988561 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    // await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'); Linha de comando para instalar extensão
     await queryRunner.createTable(
       new Table({
         name: 'products',
@@ -12,7 +12,7 @@ export class CreateProducts1645046988561 implements MigrationInterface {
             type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
-            default: 'uuid_generate_v4()',
+            // default: 'uuid_generate_v4()', Náo da para usar pois não foi possível instalar extensão uuid-ossp
           },
           {
             name: 'name',
