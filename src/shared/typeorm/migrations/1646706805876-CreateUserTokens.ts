37200579fd0407b047c02a9e24184c970271c920
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2022-03-10 19:58:37 -0300

    feature -- ForgotPassword and ResetPassword

---
+++ src/shared/typeorm/migrations/1646706805876-CreateUserTokens.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTokens1646706805876 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment',
+            isGenerated: true,
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_tokens');
+  }
+}
