8c5afbc8386915af4400abdd120c3377f7abe13e
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2023-09-19 20:13:41 -0300

    Orders Migration

---
+++ src/shared/typeorm/migrations/1695163153680-CreateOrders.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateOrders1695163153680 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment',
+            isGenerated: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
