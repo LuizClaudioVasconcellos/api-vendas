dce20ec4e837d0d9ca67afeb9eeceebddbee2451
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2023-10-05 02:35:11 -0300

    add Logout swagger doc

--- src/modules/users/routes/logout.routes.ts
+++ src/modules/users/routes/logout.routes.ts
@@ -7,6 +7,25 @@ const logoutController = new LogoutController();
 
 logoutRouter.use(isAuthenticated);
 
+/**
+ * @openapi
+ * /logout:
+ *   post:
+ *     summary: Logs out the authenticated user
+ *     tags: [Auth]
+ *     responses:
+ *        '204':
+ *          description: Logout completed successfully.
+ *        '400':
+ *          description: Bad Request
+ *        '401':
+ *          description: Unauthorized (unauthenticated user or invalid token).
+ *        '429':
+ *          description: Too Many Requests
+ *        '500':
+ *          description: Internal Server Error
+ */
+
 logoutRouter.post('/', logoutController.logout);
 
 export default logoutRouter;
